ed6b31de71d519b82634c2bb0e7c0eaaa2216c96
JanMorgenweck <dj.blacky84@googlemail.com>
2024-04-26 16:06:06 +0200

    26.04. 16:04 Uhr
    verschiedene Fragment noch angelegt, kleiner Änderungen vorgenommen.
    
    26.04. 14:08 Uhr
    
    UeberUnsFragment, fragment_ueber_uns.xml, DatenschutzFragment, fragment_datenschutz.xml angelegt und eingebunden.
    
    25.04. 16:26Uhr
    TODO:
    nichts verändert, konnte das Problem noch nicht beheben

--- app/src/main/java/com/example/bsgkaliwerratiefenort/ui/ProfilFragment.kt
+++ app/src/main/java/com/example/bsgkaliwerratiefenort/ui/ProfilFragment.kt
@@ -2,16 +2,20 @@ package com.example.bsgkaliwerratiefenort.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import coil.load
 import com.example.bsgkaliwerratiefenort.FirebaseViewModel
+import com.example.bsgkaliwerratiefenort.R
 import com.example.bsgkaliwerratiefenort.data.model.Profile
 import com.example.bsgkaliwerratiefenort.databinding.FragmentProfilBinding
 
@@ -77,6 +81,69 @@ class ProfilFragment : Fragment() {
                 binding.tvEmail.text = it.email
             }
         }
+        binding.arrowBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.homeicon.setOnClickListener {
+            findNavController().navigate(R.id.startseiteFragment)
+        }
+
+        binding.ivMenu.setOnClickListener {
+            showPopupMenu()
+        }
+    }
+    private fun showPopupMenu(){
+        val popupMenu = PopupMenu(requireContext(),binding.ivMenu)
+        popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+
+
+
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.action_startseite -> {
+                    findNavController().navigate(R.id.startseiteFragment)
+                    true
+                }
+                R.id.action_Neuigkeiten -> {
+                    findNavController().navigate(R.id.neuigkeitenFragment)
+                    true
+                }
+                R.id.action_verein -> {
+                    findNavController().navigate(R.id.vereinFragment)
+                    true
+                }
+                R.id.action_manschaften -> {
+                    findNavController().navigate(R.id.mannschaftFragment)
+                    true
+                }
+                R.id.action_ueberUns -> {
+                    findNavController().navigate(R.id.ueberUnsFragment)
+                    true
+                }
+                R.id.action_sponsorenPartner -> {
+                    findNavController().navigate(R.id.sponsorenFragment)
+                    true
+                }
+                R.id.action_onlineShop -> {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://kali-werra-shop.de"))
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_profil -> {
+                    findNavController().navigate(R.id.profilFragment)
+                    true
+                }
+                R.id.action_favoriten -> {
+                    findNavController().navigate(R.id.favoritenFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+        popupMenu.show()
     }
 
     private fun selectImage() {
