003fb19a20b1ced7187a669929cac2c9439ea709
JanMorgenweck <dj.blacky84@googlemail.com>
2024-05-17 15:13:41 +0200

    Stand 17.05.2024 15:10 Uhr
    Menü ausgelager in activity_main.xml

--- app/src/main/java/com/example/bsgkaliwerratiefenort/ui/VereinFragment.kt
+++ app/src/main/java/com/example/bsgkaliwerratiefenort/ui/VereinFragment.kt
@@ -1,7 +1,6 @@
 package com.example.bsgkaliwerratiefenort.ui
 
 import android.content.Intent
-import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.MediaController
-import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
@@ -27,7 +25,7 @@ class VereinFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentVereinBinding.inflate(inflater, container, false)
         return binding.root
 
