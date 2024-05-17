ada145b350678c81eaf6660fcc2bda608a4a5752
JanMorgenweck <dj.blacky84@googlemail.com>
2024-05-17 15:10:25 +0200

    Stand 17.05.2024 15:10 Uhr
    Menü ausgelager in activity_main.xml

--- app/src/main/java/com/example/bsgkaliwerratiefenort/MainActivity.kt
+++ app/src/main/java/com/example/bsgkaliwerratiefenort/MainActivity.kt
@@ -6,10 +6,8 @@ import android.os.Bundle
 import android.widget.PopupMenu
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isGone
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import coil.load
 import com.example.bsgkaliwerratiefenort.databinding.ActivityMainBinding
 import com.example.kaliwerra.data.Datasource
