6785ebf0382de6180d69880dfd921cd18bb94718
Jan Morgenweck <152178036+JanMorgenweck@users.noreply.github.com>
2024-09-27 09:33:51 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,44 +39,47 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("debug") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            buildConfigField("String", "API_URL", "\"https://api.example.com/debug\"")
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            buildConfigField("String", "API_URL", "\"https://api.example.com/release\"")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "19"
     }
 
     buildFeatures {
         viewBinding = true
-
+        buildConfig = true
     }
 }
 
 dependencies {
-
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     val retrofitVersion = "2.9.0"
     val roomVersion = "2.6.0"
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     //Retrofit und Moshi
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
@@ -95,7 +98,7 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     //Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
 
 
     //TODO: Add the dependencies for Firebase products you want to use
@@ -115,12 +118,12 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-storage:7.2.0")
 
     //Recyclerview
-    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
 
     // Firebase Realtime Database
-    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
 
 }
